0ecdc88824039adc1a429268e25ddb2ae684893e
vlavik007 <vlavik007@gmail.com>
2018-05-30 01:26:14 +0300

    First version of the app's controller is added using TDD

---
+++ src/main/kotlin/com/vlavik/urlshortener/controller/RedirectController.kt
@@ -0,0 +1,26 @@
+package com.vlavik.urlshortener.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import javax.servlet.http.HttpServletResponse
+
+@Controller
+@RequestMapping("/{key}")
+class RedirectController {
+
+    @RequestMapping()
+    fun redirect(@PathVariable("key") key: String, response: HttpServletResponse) {
+        if (key == "aAbBcCdD") {
+            response.setHeader(HEADER_NAME, "http://www.eveonline.com")
+            response.status = 302
+        } else {
+            response.sendError(404)
+        }
+    }
+
+    companion object {
+        private val HEADER_NAME = "Location"
+    }
+}
+
---
+++ src/test/kotlin/com/vlavik/urlshortener/controller/RedirectControllerTest.kt
@@ -0,0 +1,52 @@
+package com.vlavik.urlshortener.controller
+
+import com.vlavik.urlshortener.UrlShortenerApplication
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.header
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+@RunWith(SpringJUnit4ClassRunner::class)
+@SpringBootTest(classes = arrayOf(UrlShortenerApplication::class))
+@WebAppConfiguration
+class RedirectControllerTest {
+
+    @Autowired lateinit var webApplicationContext: WebApplicationContext
+
+    lateinit var mockMvc: MockMvc
+
+    @Before
+    fun init() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
+    }
+
+    private val PATH = "/aAbBcCdD"
+    private val REDIRECT_STATUS: Int = 302
+    private val HEADER_NAME = "Location"
+    private val HEADER_VALUE = "http://www.eveonline.com"
+
+    @Test
+    fun controllerMustRedirectUsWhenRequestIsSuccessful() {
+        mockMvc.perform(get(PATH))
+                .andExpect(status().`is`(REDIRECT_STATUS))
+                .andExpect(header().string(HEADER_NAME, HEADER_VALUE))
+    }
+
+    private val BAD_PATH = "/sadsd"
+    private val NOT_FOUND: Int = 404
+
+    @Test
+    fun controllerMustReturn404IfBadKey() {
+        mockMvc.perform(get(BAD_PATH))
+                .andExpect(status().`is`(NOT_FOUND))
+    }
+}
