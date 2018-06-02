d58c04bb2c7a2d02c5cfd294b5de288ad3515b4b
vlavik007 <vlavik007@gmail.com>
2018-06-02 13:49:33 +0300

    Upgraded DefaultKeyMapperService and its tests to work with repository

--- src/main/kotlin/com/vlavik/urlshortener/service/impl/DefaultKeyMapperService.kt
+++ src/main/kotlin/com/vlavik/urlshortener/service/impl/DefaultKeyMapperService.kt
@@ -1,36 +1,34 @@
 package com.vlavik.urlshortener.service.impl
 
+import com.vlavik.urlshortener.model.Link
+import com.vlavik.urlshortener.model.repository.LinkRepository
 import com.vlavik.urlshortener.service.KeyConverterService
 import com.vlavik.urlshortener.service.KeyMapperService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.atomic.AtomicLong
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class DefaultKeyMapperService : KeyMapperService {
 
-    private val map: MutableMap<Long, String> = ConcurrentHashMap()
-
     @Autowired
     lateinit var converter: KeyConverterService
 
-    val sequence = AtomicLong(10_000_000L)
+    @Autowired
+    lateinit var repository: LinkRepository
 
-    override fun add(link: String): String {
-        val id = sequence.getAndIncrement()
-        val key = converter.idToKey(id)
-        map[id] = link
-        return key
-    }
+    @Transactional
+    override fun add(link: String) =
+            converter.idToKey(repository.save(Link(link)).id)
 
     override fun getLink(key: String): KeyMapperService.Get {
         val id = converter.keyToId(key)
-        val result = map[id]
-        if (result == null) {
-            return KeyMapperService.Get.NotFound(key)
+        val result = repository.findById(id)
+
+        return if (result.isPresent) {
+            KeyMapperService.Get.Link(result.get().text)
         } else {
-            return KeyMapperService.Get.Link(result)
+            return KeyMapperService.Get.NotFound(key)
         }
     }
 }
--- src/test/kotlin/com/vlavik/urlshortener/service/DefaultKeyMapperServiceTest.kt
+++ src/test/kotlin/com/vlavik/urlshortener/service/DefaultKeyMapperServiceTest.kt
@@ -1,6 +1,9 @@
 package com.vlavik.urlshortener.service
 
+import com.vlavik.urlshortener.model.Link
+import com.vlavik.urlshortener.model.repository.LinkRepository
 import com.vlavik.urlshortener.service.impl.DefaultKeyMapperService
+import com.vlavik.urlshortener.whenever
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotEquals
 import org.junit.Before
@@ -9,23 +12,30 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import java.util.*
 
 class DefaultKeyMapperServiceTest {
 
     @InjectMocks
     private val service: KeyMapperService = DefaultKeyMapperService()
 
-    private val KEY: String = "aAbBcCdD"
-    private val LINK_A: String = "https://www.google.com"
-    private val LINK_B: String = "https://vk.com"
-
     @Mock
     lateinit var converter: KeyConverterService
 
+    @Mock
+    lateinit var repository: LinkRepository
+
+    private val KEY: String = "aAbBcCdD"
+
     private val KEY_A: String = "abc"
-    private val KEY_B: String = "cde"
+    private val LINK_A: String = "https://www.google.com"
     private val ID_A: Long = 10_000_000L
+    private val LINK_OBJ_A: Link = Link(LINK_A, ID_A)
+
+    private val KEY_B: String = "cde"
+    private val LINK_B: String = "https://vk.com"
     private val ID_B: Long = 10_000_001L
+    private val LINK_OBJ_B: Link = Link(LINK_B, ID_B)
 
     @Before
     fun init() {
@@ -34,6 +44,12 @@ class DefaultKeyMapperServiceTest {
         Mockito.`when`(converter.idToKey(ID_A)).thenReturn(KEY_A)
         Mockito.`when`(converter.keyToId(KEY_B)).thenReturn(ID_B)
         Mockito.`when`(converter.idToKey(ID_B)).thenReturn(KEY_B)
+
+        whenever(repository.findById(Mockito.any())).thenReturn(Optional.empty())
+        whenever(repository.save(Link(LINK_A))).thenReturn(LINK_OBJ_A)
+        whenever(repository.save(Link(LINK_B))).thenReturn(LINK_OBJ_B)
+        whenever(repository.findById(ID_A)).thenReturn(Optional.of(LINK_OBJ_A))
+        whenever(repository.findById(ID_B)).thenReturn(Optional.of(LINK_OBJ_B))
     }
 
     @Test
